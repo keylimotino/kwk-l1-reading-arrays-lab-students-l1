8aa537f22a14b1db906aa52d2af5c1581d1d8d4d
keylimotino <stephanie48758@gmail.com>
2018-06-06 15:17:31 +0000

    automatically backed up by learn

---
+++ arrays.rb
@@ -0,0 +1,13 @@
+student_names = ["Cardi B", "Rihanna", "Nicky Minaj", "Katy Perry"]
+puts student_names.size
+
+
+student_names << "Rihanna"
+
+current_idx = 0
+
+while current_idx < student_names.size
+  current_idx = student_names[current_idx]
+  puts 
+
+end
